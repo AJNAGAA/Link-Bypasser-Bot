816b776a005a28b07f6f929880a4c7d8343c12f0
AJNAGAA <101543053+AJNAGAA@users.noreply.github.com>
2024-01-25 16:58:10 +0530

    Delete config.py

--- config.py
+++
@@ -1,5 +0,0 @@
-class Config(object):
-   # Url Shortner Information 
-    TECH_VJ = bool(environ.get('TECH_VJ', True)) # Set False If you want shortlink off else True
-    TECH_VJ_URL = environ.get('TECH_VJ_URL', 'moneykamalo.com') # your shortlink url domain or url without https://
-    TECH_VJ_API = environ.get('TECH_VJ_API', '0eefb93e1e3ce9470a7033115ceb1bad13a9d674') # your url shortner api
